d4d2b314cd14837f360c378f0d4e731931ef79d3
Briggs Elsperger <briggs.elsperger@hotmail.com>
2017-08-25 21:51:05 -0500

    lkj

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper
+
   def index
     @articles = Article.all
   end
@@ -11,7 +13,25 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
-   def create
-  #   # fail
-   end
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to article_path(@article)
+  end
+  
+  def destroy
+    @article = Article.destroy(params[:id])
+    redirect_to articles_path(@articles)
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
+    redirect_to article_path(@article)
+  end
 end
